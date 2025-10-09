7b5c699be8fa44705f0a92a54d3430d49ed1aa13
David-Silva <orpi4488@gmail.com>
2025-10-08 20:00:42 -0500

    warehose fix

--- database/Warehouse.cs
+++ database/Warehouse.cs
@@ -7,9 +7,90 @@ namespace Healt_Clinict.database;
 public static class Warehouse
 {
     public static List<Veterinarian> veterinarians = [];
-    public static List<Customer> customers = new()
+    public static List<Customer> customers = [];
+    
+        // new Customer
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Name = "Carlos",
+        //     LastName = "Ramírez",
+        //     Age = 28,
+        //     TypeDocument = "CC",
+        //     NumberDocument = "1002456789",
+        //     //Email = "carlos.ramirez@example.com",
+        //     //PhoneNumber = "3004567890",
+        //     Pets = new List<Pet>()
+        //     {
+        //         new Pet("Firulais", "Perro", "Macho", 3, "Marrón", "15kg", "Sano", null!)
+        //     }
+        // },
+        // new Customer
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Name = "Laura",
+        //     LastName = "Gómez",
+        //     Age = 34,
+        //     TypeDocument = "CC",
+        //     NumberDocument = "1003987654",
+        //     //Email = "laura.gomez@example.com",
+        //     // PhoneNumber = "3019876543",
+        //     Pets = new List<Pet>()
+        //     {
+        //         new Pet("Michi", "Gato", "Hembra", 2, "Blanco", "4kg", "Resfriado leve", null!)
+        //     }
+        // },
+        // new Customer
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Name = "Andrés",
+        //     LastName = "Torres",
+        //     Age = 22,
+        //     TypeDocument = "TI",
+        //     NumberDocument = "1122334455",
+        //     //Email = "andres.torres@example.com",
+        //     //PhoneNumber = "3021234567",
+        //     Pets = new List<Pet>()
+        //     {
+        //         new Pet("Rex", "Perro", "Macho", 5, "Negro", "20kg", "Cojea", null!)
+        //     }
+        // },
+        // new Customer
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Name = "Valentina",
+        //     LastName = "Martínez",
+        //     Age = 29,
+        //     TypeDocument = "CC",
+        //     NumberDocument = "1005678912",
+        //     //Email = "valentina.martinez@example.com",
+        //     //PhoneNumber = "3156789123",
+        //     Pets = new List<Pet>()
+        //     {
+        //         new Pet("Luna", "Conejo", "Hembra", 1, "Gris", "1.5kg", "Sano", null!)
+        //     }
+        // },
+        // new Customer
+        // {
+        //     Id = Guid.NewGuid(),
+        //     Name = "Miguel",
+        //     LastName = "Suárez",
+        //     Age = 40,
+        //     TypeDocument = "CE",
+        //     NumberDocument = "890123456",
+        //     //Email = "miguel.suarez@example.com",
+        //     //PhoneNumber = "3168901234",
+        //     Pets = new List<Pet>()
+        //     {
+        //         new Pet("Rocky", "Perro", "Macho", 7, "Café", "25kg", "Dolor en la pata", null!)
+        //     }
+        // }
+
+
+    
+
+    static Warehouse()
     {
-        new Customer
+        var cliente1 = new Customer
         {
             Id = Guid.NewGuid(),
             Name = "Carlos",
@@ -19,12 +100,13 @@ public static class Warehouse
             NumberDocument = "1002456789",
             //Email = "carlos.ramirez@example.com",
             //PhoneNumber = "3004567890",
-            Pets = new List<Pet>()
-            {
-                new Pet("Firulais", "Perro", "Macho", 3, "Marrón", "15kg", "Sano", null!) 
-            }
-        },
-        new Customer
+            Pets = []
+        };
+
+
+        var mascota1 = new Pet("Firulais", "Perro", "Macho", 3, "Marrón", "15kg", "Sano", cliente1);
+
+        var cliente2 = new Customer
         {
             Id = Guid.NewGuid(),
             Name = "Laura",
@@ -34,57 +116,11 @@ public static class Warehouse
             NumberDocument = "1003987654",
             //Email = "laura.gomez@example.com",
             // PhoneNumber = "3019876543",
-            Pets = new List<Pet>()
-            {
-                new Pet("Michi", "Gato", "Hembra", 2, "Blanco", "4kg", "Resfriado leve", null!)
-            }
-        },
-        new Customer
-        {
-            Id = Guid.NewGuid(),
-            Name = "Andrés",
-            LastName = "Torres",
-            Age = 22,
-            TypeDocument = "TI",
-            NumberDocument = "1122334455",
-            //Email = "andres.torres@example.com",
-            //PhoneNumber = "3021234567",
-            Pets = new List<Pet>()
-            {
-                new Pet("Rex", "Perro", "Macho", 5, "Negro", "20kg", "Cojea", null!)
-            }
-        },
-        new Customer
-        {
-            Id = Guid.NewGuid(),
-            Name = "Valentina",
-            LastName = "Martínez",
-            Age = 29,
-            TypeDocument = "CC",
-            NumberDocument = "1005678912",
-            //Email = "valentina.martinez@example.com",
-            //PhoneNumber = "3156789123",
-            Pets = new List<Pet>()
-            {
-                new Pet("Luna", "Conejo", "Hembra", 1, "Gris", "1.5kg", "Sano", null!)
-            }
-        },
-        new Customer
-        {
-            Id = Guid.NewGuid(),
-            Name = "Miguel",
-            LastName = "Suárez",
-            Age = 40,
-            TypeDocument = "CE",
-            NumberDocument = "890123456",
-            //Email = "miguel.suarez@example.com",
-            //PhoneNumber = "3168901234",
-            Pets = new List<Pet>()
-            {
-                new Pet("Rocky", "Perro", "Macho", 7, "Café", "25kg", "Dolor en la pata", null!)
-            }
-        }
+            Pets = []
 
+        };
 
-    };
+        var mascota2 = new Pet("Michi", "Gato", "Hembra", 2, "Blanco", "4kg", "Resfriado leve", cliente2);
+    }
+
 }
