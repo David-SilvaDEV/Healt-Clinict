d635c134e4123394cbbb465bd2373382d68f8106
David-Silva <orpi4488@gmail.com>
2025-10-10 15:22:08 -0500

    feet: creating list citas in veterinarias

--- ClassesSupport/AppointmentCalendar.cs
+++ ClassesSupport/AppointmentCalendar.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Globalization;
-using System.Linq;
+    using System;
+    using System.Globalization;
+    using System.Linq;
 
-namespace Healt_Clinict.ClassesSupport
-{
-    public class AppointmentCalendar
+    namespace Healt_Clinict.ClassesSupport
     {
-        private AppointmentSystem appointmentSystem;
-
-        public AppointmentCalendar(AppointmentSystem appointmentSystem)
+        public class AppointmentCalendar
         {
-            this.appointmentSystem = appointmentSystem;
-        }
+            private AppointmentSystem appointmentSystem;
 
-        // Mostrar el calendario de un mes específico
-        public void DisplayCalendar(int month, int year)
-        {
-            DateTime firstDayOfMonth = new DateTime(year, month, 1);
-            int daysInMonth = DateTime.DaysInMonth(year, month);
-
-            Console.WriteLine($"{firstDayOfMonth.ToString("MMMM yyyy", CultureInfo.InvariantCulture)}");
-            Console.WriteLine("Sun|Mon|Tue|Wed|Thu|Fri|Sat");
-
-            int firstDayOfWeek = (int)firstDayOfMonth.DayOfWeek;
-            if (firstDayOfWeek == 0) firstDayOfWeek = 7;
-
-            // Imprimir espacios hasta el primer día del mes
-            for (int i = 1; i < firstDayOfWeek; i++)
+            public AppointmentCalendar(AppointmentSystem appointmentSystem)
             {
-                Console.Write("    ");
+                this.appointmentSystem = appointmentSystem;
             }
 
-            // Mostrar los días del mes
-            for (int day = 1; day <= daysInMonth; day++)
+            // Mostrar el calendario de un mes específico
+            public void DisplayCalendar(int month, int year)
             {
-                Console.Write($"{day,3} ");
+                DateTime firstDayOfMonth = new DateTime(year, month, 1);
+                int daysInMonth = DateTime.DaysInMonth(year, month);
 
-                if ((firstDayOfWeek + day - 1) % 7 == 0)
+                Console.WriteLine($"{firstDayOfMonth.ToString("MMMM yyyy", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Sun|Mon|Tue|Wed|Thu|Fri|Sat");
+
+                int firstDayOfWeek = (int)firstDayOfMonth.DayOfWeek;
+                if (firstDayOfWeek == 0) firstDayOfWeek = 7;
+
+                // Imprimir espacios hasta el primer día del mes
+                for (int i = 1; i < firstDayOfWeek; i++)
                 {
-                    Console.WriteLine();
+                    Console.Write("    ");
                 }
-            }
 
-            Console.WriteLine();
+                // Mostrar los días del mes
+                for (int day = 1; day <= daysInMonth; day++)
+                {
+                    Console.Write($"{day,3} ");
+
+                    if ((firstDayOfWeek + day - 1) % 7 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.WriteLine();
+            }
         }
     }
-}
--- Models/Veterinarian.cs
+++ Models/Veterinarian.cs
@@ -1,8 +1,11 @@
+using Healt_Clinict.ClassesSupport;
 using Healt_Clinict.obj.Models;
+using Healt_Clinict.Services;
 
-namespace Healt_Clinict.Models;
-
-public class Veterinarian : Person
+namespace Healt_Clinict.Models
 {
-    
+    public class Veterinarian : Person
+    {
+        public List<Appointment> appointments { get; set; }
+    }
 }
